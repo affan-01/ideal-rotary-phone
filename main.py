7f7a5bdeda1efae843da290a2dcbb20b28101ec6
Mohammed Affan <affan.noor35@gmail.com>
2022-09-13 19:14:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from maze import*
 
 def main():
     win = Window(800, 600)
-    maze = Maze(1,1,10,10,10,10,20,20,win)
+    maze = Maze(1,1,10,10,10,10,50,50,win)
     maze._create_cells()
-
+    maze._break_walls(0,0)
 main()
